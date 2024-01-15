f88705605352802993691a67d58e5bd6a5c1285a
labas02 <zdenda.cejka112@seznam.cz>
2024-01-15 16:24:08 +0100

    sdfsdf

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-bool movement_allowed(sf::Vector2f);
+bool movement_allowed(sf::Vector2f,sf::Vector2f);
+void render_apples();
+sf::Vector2f vector_arr[10];
+
+
 int main(int argc, char** argv) {
     sf::ContextSettings settings;
     settings.depthBits = 24;
@@ -11,23 +15,23 @@ int main(int argc, char** argv) {
     settings.antialiasingLevel = 4;
     settings.majorVersion = 3;
     settings.minorVersion = 3;
-    sf::Window window(sf::VideoMode(1920, 1020), "SFML OpenGL", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080),"SFML OpenGL", sf::Style::Fullscreen, settings);
     window.setActive(true);
-    glEnable(GL_TEXTURE_2D); 
+    glEnable(GL_TEXTURE_2D);
+
     float speed = 0.5f;
     float posX = 0.0f, posY = 0.0f;
-    float velX = speed, velY = 0.0f; // Start moving right by default
+    sf::Vector2f velocity(0.0f, 0.0f); // Start with no movement
 
     sf::Clock clock;
 
-    while(window.isOpen()) {
+    while (window.isOpen()) {
         sf::Time deltaTime = clock.restart();
         float dt = deltaTime.asSeconds();
 
         sf::Event event;
         while (window.pollEvent(event)) {
-
-            switch(event.type){
+            switch (event.type) {
                 case sf::Event::Closed:
                     window.close();
                     break;
@@ -35,23 +39,22 @@ int main(int argc, char** argv) {
                     glViewport(0, 0, event.size.width, event.size.height);
                     break;
                 case sf::Event::KeyPressed:
-                    switch(event.key.code)
-                    {
+                    switch (event.key.code) {
                         case sf::Keyboard::W:
-                            velX = 0.0f;
-                            velY = speed;
+                            velocity.x = 0.0f;
+                            velocity.y = speed;
                             break;
                         case sf::Keyboard::S:
-                            velX = 0.0f;
-                            velY = -speed;
+                            velocity.x = 0.0f;
+                            velocity.y = -speed;
                             break;
                         case sf::Keyboard::A:
-                            velX = -speed;
-                            velY = 0.0f;
+                            velocity.x = -speed;
+                            velocity.y = 0.0f;
                             break;
                         case sf::Keyboard::D:
-                            velX = speed;
-                            velY = 0.0f;
+                            velocity.x = speed;
+                            velocity.y = 0.0f;
                             break;
                         default:
                             break;
@@ -62,40 +65,75 @@ int main(int argc, char** argv) {
             }
         }
 
-        posX += velX * dt;
-        posY += velY * dt;
-        sf::Vector2f v1(posX,posY);
-	if(movement_allowed(v1))
-	{
-	window.close();
-	}
+        // Update position based on velocity
+        posX += velocity.x * dt;
+        posY += velocity.y * dt;
 
+        // Check if movement is allowed
+        if (movement_allowed(sf::Vector2f(posX, posY),sf::Vector2f(window.getSize().x,window.getSize().y))) {
+            window.close();
+        }
+
+        // Clear the buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glLoadIdentity();
+        glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+        glOrtho(-1.0, 1.0, -1.0 * window.getSize().y / window.getSize().x, 1.0 * window.getSize().y / window.getSize().x, -1.0, 1.0);
+        // Draw the square
+        glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
         glTranslatef(posX, posY, 0.0f);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(0.0, 0.1);
-        glVertex2f(-0.1, -0.1);
-        glVertex2f(0.1, -0.1);
+        glBegin(GL_QUADS);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(0.05f, 0.05f);
+        glVertex2f(-0.05f, 0.05f);
+        glVertex2f(-0.05f, -0.05f);
+        glVertex2f(0.05f, -0.05f);
         glEnd();
+        glPopMatrix();
 
-        glFlush();
+
+	render_apples();
+        // Display the rendered content
         window.display();
     }
 
     return 0;
 }
 
-
-bool movement_allowed(sf::Vector2f pos)
-{ 
-
-  if(pos.x>1||pos.x<-1||pos.y>1||pos.y<-1)
-  {
-  std::cout<<"true\n x: "<<pos.x<<"]\nY: "<<pos.y;       
-   return true;
-  }
-  std::cout<<"false\n x: "<<pos.x<<"]\nY: "<<pos.y; 
-  return false;
+bool movement_allowed(sf::Vector2f pos, sf::Vector2f border) {
+    if (pos.x > border.x/1000 || pos.x <  -border.x/1000 || pos.y > border.y/1000 || pos.y < -border.y/1000) {
+       std::cout << "true\n x: " << border.x/1000 << "\nY: " << border.y/1000;
+        return true;
+    }
+    std::cout << "false\n x: " << border.x/1000 << "\nY: " << border.y/1000;
+    return false;
 }
+
+void collect_apple(){
+
+}
+
+void render_apples(){
+
+vector_arr[1] = sf::Vector2f(0.8,0.9);
+ //std::cout<<"yello: " <<vector_arr[1].x<<"\n";
+ //std::cout<<"yello: " <<vector_arr[1].y<<"\n";
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(vector_arr[1].x,vector_arr[1].y, 0.0f);
+	glBegin(GL_QUADS);
+	glColor3f(1.0f,0.0f,0.0f);
+	glVertex2f(0.02f, 0.02f);
+        glVertex2f(-0.02f, 0.02f);
+        glVertex2f(-0.02f, -0.02f);
+        glVertex2f(0.02f, -0.02f);
+	glEnd();
+	glPopMatrix();
+}
+
